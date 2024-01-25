f9f9c9b1dbdc53106aac587d33480b95583097f1
StenKuusk <114585896+StenKuusk@users.noreply.github.com>
2024-01-25 16:31:25 +0200

    Delete the Todo by id

---
+++ src/controllers/todos.ts
@@ -0,0 +1,68 @@
+import { Request, Response, NextFunction } from "express"
+import { Todo } from '../models/todo'
+
+const todos: Todo[] = []
+
+export const createTodo = (req: Request, res:Response, next:NextFunction) => {
+    try{
+        const task = (req.body as {task: string}).task
+        const newTodo = new Todo(Math.random().toString(), task)
+        todos.push(newTodo)
+        res.status(201).json({
+            message: 'Created new todo',
+            createdTask: newTodo
+        }) 
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+export const getTodos = (req: Request, res:Response, next: NextFunction) => {
+    try{
+        res.status(201).json({
+            tasks: todos
+        })
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+export const updateTodo = (req: Request, res:Response, next: NextFunction) => {
+    try{
+        const todoId = req.params.id
+        const updatedTask = (req.body as {task: string}).task
+        const todoIndex = todos.findIndex(todo => todo.id === todoId)
+
+        if(todoIndex < 0) {
+            throw new Error('Could not find todo with such id')
+        }
+
+        todos[todoIndex] = new Todo(todos[todoIndex].id, updatedTask)
+
+        res.status(201).json({
+            message: 'Todo is updated!',
+            updatedTask: todos[todoIndex]
+        })
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+export const deleteTodo = (req: Request, res:Response, next:NextFunction) => {
+    try{
+        const todoId = req.params.id
+        const todoIndex = todos.findIndex(todo => todo.id === todoId)
+
+        if(todoIndex < 0) {
+            throw new Error('Could not find todo with such id')
+        }
+
+        todos.splice(todoIndex, 1)
+
+        res.status(201).json({
+            message: 'Todo is deleted!',
+        })
+    } catch(error) {
+        console.log(error)
+    }
+}
